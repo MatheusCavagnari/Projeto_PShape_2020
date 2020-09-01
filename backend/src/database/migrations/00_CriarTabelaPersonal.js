af025340d673f0a67ea220e0d67980cc57b8cb4e
Katriel Koga <katriel.koga.2012@hotmail.com>
2020-08-31 21:13:51 -0300

    criando migrations personal e alunos e possivel calendario para utilizar (utils)

---
+++ backend/src/database/migrations/00_CriarTabelaPersonal.js
@@ -0,0 +1,14 @@
+exports.up = function(knex) {
+    // CRIAR A TABELA
+    return knex.schema.createTable('personal', table => {
+      table.increments('id').primary();
+      table.string('nome').notNullable();
+      table.string('email').notNullable().unique();
+      table.string('senha').notNullable();
+    })
+}
+
+exports.down = function(knex) {
+    // VOLTAR ATRAS (DELETAR A TABELA)
+    return knex.schema.dropTable('personal');
+}
