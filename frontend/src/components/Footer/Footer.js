2165c723d536fc4cd356354b6773a1bf2a52b776
Katriel <katriel.koga.2012@hotmail.com>
2021-02-06 09:00:32 -0300

    Alteraçao front Katriel

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+
+
+export default function Footer () {
+    return (
+      <div className="footer">
+          <h2 id="footerTitulo"> (42) 3223-3457 </h2>
+      </div>
+    )
+  }
