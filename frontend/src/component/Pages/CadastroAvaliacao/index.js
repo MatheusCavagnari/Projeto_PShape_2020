2af6d52356641c0fadf62c32af75266bbcaff7a5
unknown <matheuscavagnari77@gmail.com>
2021-03-25 11:18:39 -0300

    cadastroAvaliacao

--- frontend/src/component/Pages/CadastroAvaliacao/index.js
+++ frontend/src/component/Pages/CadastroAvaliacao/index.js
@@ -1,23 +1,113 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
+import { useHistory } from 'react-router-dom'
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 
 import Header from '../../Header'
 import Footer from '../../Footer'
 import Menu from '../../Menu'
-
 import './styles.css'
+
 import { TextField } from '@material-ui/core'
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+import api from '../../../services/api'
 
 function CadastroAvaliacao() {
-    const [nome, setNome] = useState('');
-    const [abdominal, setAbdominal] = useState('');
-    const [axilar, setAxilar] = useState('');
-    const [biciptal, setBiciptal] = useState('');
-    const [coxa, setCoxa] = useState('');
-    const [panturrilha, setPanturrilha] = useState('');
-    const [subescapular, setSubescapular] = useState('');
-    const [supraIliaca, setSupraIliaca] = useState('');
-    const [toracica, setToracica] = useState('');
-    const [triciptal, setTriciptal] = useState('');
+    const [ aluno_id, setAluno] = useState('');
+    const [dobra_abdominal, setAbdominal] = useState('');
+    const [dobra_axilar, setAxilar] = useState('');
+    const [dobra_bicipal, setBiciptal] = useState('');
+    const [dobra_coxa, setCoxa] = useState('');
+    const [dobra_panturrilha, setPanturrilha] = useState('');
+    const [dobra_subescapular, setSubescapular] = useState('');
+    const [dobra_supra_iliaca, setSupraIliaca] = useState('');
+    const [dobra_toracica, setToracica] = useState('');
+    const [dobra_tricipal, setTricipal] = useState('');
+    const [peso, setPeso] = useState('');
+    const [altura, setAltura] = useState('');
+    const [data_avaliacao, setData] = useState('');
+
+
+   
+    const [alunos, setAlunos] = useState([]);
+
+
+    const history = useHistory()
+
+
+    useEffect(() => {
+
+        const avalAluno = async () => {
+            await api.get('/alunos', { headers: { personal: localStorage.getItem('personal') } })
+                .then(response => {
+                    setAlunos(response.data)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+
+        avalAluno()
+    }, [])
+    
+    function cancelar(e) {
+      e.preventDefault()
+      history.push('/')
+    }
+    
+    
+
+    async function btnAddAvalicao(e) {
+        e.preventDefault()
+
+        console.log(data_avaliacao);
+        console.log(peso);
+        console.log(dobra_tricipal);
+        console.log(dobra_bicipal);
+        console.log(aluno_id);
+        console.log(aluno_id);
+        console.log(aluno_id);
+        console.log(aluno_id);
+        console.log(aluno_id);
+
+        try{
+            await api.post(`/avaliacao`, {
+                peso, 
+                altura, 
+                dobra_tricipal, 
+                dobra_bicipal,
+                dobra_toracica,
+                dobra_panturrilha,
+                dobra_abdominal,
+                dobra_coxa,
+                dobra_subescapular,
+                dobra_supra_iliaca,
+                dobra_axilar,
+                data_avaliacao,
+                aluno_id
+            })
+      
+            
+      
+            alert(`Usuário alterado com sucesso!`)
+        //     // console.log(response.data)
+              history.push('/')
+          } catch (err) {
+            alert(`Aconteceu algum erro ${err.response.data}`)
+            console.log(err)
+          }
+
+      }
+
+      function selecionaAluno(e) {
+        const abc = e.target.id;
+        const index = abc.slice(22);
+        if(index){
+            setAluno(alunos[parseInt(index)].id)
+          } else {
+            setAluno([])
+          }
+    }
 
 
     return (
@@ -27,34 +117,62 @@ function CadastroAvaliacao() {
             <div className="main">
                 <div className="boxAlt">
                     <h2>Cadastro de Avaliacao</h2>
-                    <form action="">
-                        <TextField id="standard-basic nome"
-                            label="Nome"
-                            name="nome"
-                            required
-                            value={nome}
-                            onChange={e => setNome(e.target.value)}
+                    <form onSubmit={btnAddAvalicao}>
+                        <div>
+                        <Autocomplete
+                            id="combo-box-demo"
+                            options={alunos}
+                            getOptionLabel={(option) => option.nome}
+                            style={{ width: "50%" }}
+                            renderInput={(params) => <TextField {...params} label="Aluno" variant="outlined" />}
+                            onChange={selecionaAluno}
                         />
+                        </div>
+                   
+                    <div className="horizontalBox">
+                            <TextField id="standard-basic nome"
+                                label="Altura"
+                                name="altura"
+                                required
+                                value={altura}
+                                onChange={e => setAltura(e.target.value)}
+                            />
+                            <TextField id="standard-basic nome"
+                                label="Peso "
+                                name="peso"
+                                required
+                                value={peso}
+                                onChange={e => setPeso(e.target.value)}
+                            />
+                            <TextField id="standard-basic nome"
+                                label="Data"
+                                name="data"
+                                required
+                                value={data_avaliacao}
+                                onChange={e => setData(e.target.value)}
+                            />
+
+                        </div>
                         <div className="horizontalBox">
                             <TextField id="standard-basic nome"
                                 label="Dobra Abdominal"
                                 name="abdominal"
                                 required
-                                value={abdominal}
+                                value={dobra_abdominal}
                                 onChange={e => setAbdominal(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
                                 label="Dobra Axilar "
                                 name="axilar"
                                 required
-                                value={axilar}
+                                value={dobra_axilar}
                                 onChange={e => setAxilar(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
                                 label="Dobra Biciptal"
                                 name="biciptal"
                                 required
-                                value={biciptal}
+                                value={dobra_bicipal}
                                 onChange={e => setBiciptal(e.target.value)}
                             />
 
@@ -64,21 +182,21 @@ function CadastroAvaliacao() {
                                 label="Dobra da Coxa"
                                 name="coxa"
                                 required
-                                value={coxa}
+                                value={dobra_coxa}
                                 onChange={e => setCoxa(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
                                 label="Dobra Panturrilha"
                                 name="panturrilha"
                                 required
-                                value={panturrilha}
+                                value={dobra_panturrilha}
                                 onChange={e => setPanturrilha(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
                                 label="Dobra Subescapular"
                                 name="subescapular"
                                 required
-                                value={subescapular}
+                                value={dobra_subescapular}
                                 onChange={e => setSubescapular(e.target.value)}
                             />
                         </div>
@@ -88,27 +206,27 @@ function CadastroAvaliacao() {
                                 label="Dobra Supra-ilíaca"
                                 name="supraIliaca"
                                 required
-                                value={supraIliaca}
+                                value={ dobra_supra_iliaca}
                                 onChange={e => setSupraIliaca(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
                                 label="Dobra Torácica"
                                 name="toracica"
                                 required
-                                value={toracica}
+                                value={dobra_toracica}
                                 onChange={e => setToracica(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
-                                label="Dobra Triciptal"
-                                name="triciptal"
+                                label="Dobra Tricipal"
+                                name="tricipal"
                                 required
-                                value={triciptal}
-                                onChange={e => setTriciptal(e.target.value)}
+                                value={dobra_tricipal}
+                                onChange={e => setTricipal(e.target.value)}
                             />
                         </div>
                         <div className="horizontalBox buttons">
                             <button>Cadastrar</button>
-                            <button className="cancel">Cancelar</button>
+                            <button onClick={cancelar} className="cancel">Cancelar</button>
                         </div>
                     </form>
                 </div>
