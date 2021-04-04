617809730e605464559f069b8693255e8799b897
unknown <matheuscavagnari77@gmail.com>
2021-04-04 19:05:58 -0300

    type number

--- frontend/src/component/Pages/CadastroAvaliacao/index.js
+++ frontend/src/component/Pages/CadastroAvaliacao/index.js
@@ -110,7 +110,7 @@ function CadastroAvaliacao() {
             <Menu page="1" />
             <div className="main">
                 <div className="boxAlt">
-                    <h2>Cadastro de Avaliacao</h2>
+                    <h2>Cadastro de Avaliação</h2>
                     <form onSubmit={btnAddAvalicao}>
                         <div > 
                         <Autocomplete
@@ -118,24 +118,26 @@ function CadastroAvaliacao() {
                             options={alunos}
                             getOptionLabel={(option) => option.nome}
                             
-                            renderInput={(params) => <TextField {...params} label="Aluno" variant="outlined" />}
+                            renderInput={(params) => <TextField {...params} label="Aluno*" variant="outlined" />}
                             onChange={selecionaAluno}
                         />
                         </div>
                    
                     <div className="horizontalBox">
                             <TextField id="standard-basic nome"
-                                label="Altura"
+                                label="Altura (m)"
                                 name="altura"
                                 required
                                 value={altura}
+                                type="number"
                                 onChange={e => setAltura(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
-                                label="Peso "
+                                label="Peso (Kg)"
                                 name="peso"
                                 required
                                 value={peso}
+                                type="number"
                                 onChange={e => setPeso(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
@@ -154,72 +156,81 @@ function CadastroAvaliacao() {
                         </div>
                         <div className="horizontalBox">
                             <TextField id="standard-basic nome"
-                                label="Dobra Abdominal"
+                                label="Dobra Abdominal (mm)"
                                 name="abdominal"
                                 required
                                 value={dobra_abdominal}
+                                type="number"
                                 onChange={e => setAbdominal(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
-                                label="Dobra Axilar "
+                                label="Dobra Axilar (mm)"
                                 name="axilar"
                                 required
                                 value={dobra_axilar}
+                                type="number"
                                 onChange={e => setAxilar(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
-                                label="Dobra Biciptal"
+                                label="Dobra Biciptal (mm)"
                                 name="biciptal"
                                 required
                                 value={dobra_bicipal}
+                                type="number"
                                 onChange={e => setBiciptal(e.target.value)}
                             />
 
                         </div>
                         <div className="horizontalBox">
                             <TextField id="standard-basic nome"
-                                label="Dobra da Coxa"
+                                label="Dobra Coxa (mm)"
                                 name="coxa"
                                 required
                                 value={dobra_coxa}
+                                type="number"
                                 onChange={e => setCoxa(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
-                                label="Dobra Panturrilha"
+                                label="Dobra Panturrilha (mm)"
                                 name="panturrilha"
                                 required
                                 value={dobra_panturrilha}
+                                type="number"
                                 onChange={e => setPanturrilha(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
-                                label="Dobra Subescapular"
+                                label="D Subescapular(mm)"
                                 name="subescapular"
                                 required
                                 value={dobra_subescapular}
+                                type="number"
                                 onChange={e => setSubescapular(e.target.value)}
                             />
                         </div>
 
                         <div className="horizontalBox">
                             <TextField id="standard-basic nome"
-                                label="Dobra Supra-ilíaca"
+                                label="D Supra-ilíaca (mm)"
                                 name="supraIliaca"
                                 required
                                 value={ dobra_supra_iliaca}
+                                type="number"
                                 onChange={e => setSupraIliaca(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
-                                label="Dobra Torácica"
+                                label="Dobra Torácica (mm)"
                                 name="toracica"
                                 required
                                 value={dobra_toracica}
+                                type="number"
                                 onChange={e => setToracica(e.target.value)}
                             />
                             <TextField id="standard-basic nome"
-                                label="Dobra Tricipal"
+                                label="Dobra Tricipal "
                                 name="tricipal"
                                 required
                                 value={dobra_tricipal}
+                                type="number"
                                 onChange={e => setTricipal(e.target.value)}
                             />
                         </div>
